f5efa87bea71d94558c83671bbbc99b8bcf4f80e
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-08-16 11:25:46 +0100

    feat: implemented bottom navbar and navigation to other pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'pages/blog.dart';
+import 'pages/recommend.dart';
+import 'pages/search.dart';
+import 'pages/my_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +12,74 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'ViewerVault'),
+      home: const MyHomePage(title: 'ViewerVault'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const BlogPage(),
+    const RecommendPage(),
+    const SearchPage(),
+    const MyListPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Blog',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Recommend',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'My List',
+          ),
+        ],
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.grey[400],
+        backgroundColor: Colors.grey[900],
+        elevation: 8,
+      ),
     );
   }
 }
--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(170, 58, 175, 185),
-        title: const Center(child: Text('Home Page')),
+        title: const Center(child: Text('Search Page')),
       ),
       body: const Center(
-        child: Text('This is the Home Page'),
+        child: Text('This is the Search Page'),
       ),
     );
   }
